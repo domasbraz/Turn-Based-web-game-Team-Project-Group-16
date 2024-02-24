f5fc79846f97f5edddcc0c825ac583d495fbc4a8
domasbraz <114153020+domasbraz@users.noreply.github.com>
2024-02-24 11:35:32 +0000

    Add files via upload

---
+++ code/js/combat.js
@@ -0,0 +1,10 @@
+function switchGuiBot()
+{
+
+    var botGUI1 = document.querySelectorAll(".b1GUI");
+
+    botGUI1.forEach(function(element) 
+    {
+        element.style.display = "none";
+    });
+}

90c6f2ac23a78ec1ae6d34ce942bf023effdb4b2
Adam Plesca <116078864+adamplesca@users.noreply.github.com>
2024-03-21 14:24:45 +0000

    Add files via upload
    temp file, has all I've done for the questboard

---
+++ code/Quest board/questBoard.js
@@ -0,0 +1,15 @@
+function easyQ(){
+    document.getElementById("e").style.visibility = 'visible';  
+    document.getElementById("m").style.visibility = 'hidden';  
+    document.getElementById("h").style.visibility = 'hidden';  
+}
+function medQ(){
+    document.getElementById("e").style.visibility = 'hidden';  
+    document.getElementById("m").style.visibility = 'visible';  
+    document.getElementById("h").style.visibility = 'hidden';  
+}
+function hardQ(){
+    document.getElementById("e").style.visibility = 'hidden';  
+    document.getElementById("m").style.visibility = 'hidden';  
+    document.getElementById("h").style.visibility = 'visible';  
+}
